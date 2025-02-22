d8d1783958ad9078697b7f5cc9650a819f8d3fa2
Cristian Nistor <cristake@mac.home>
2025-02-22 06:35:04 -0500

    practice functional

--- Learning Progress Tracker (Java)/task/src/tracker/Main.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Main.java
@@ -4,9 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Learning Progress Tracker");
 
-//        Menu.run();
-//
-//        System.out.println("Bye!");
-//        ScannerUtil.closeScanner();
+        Menu.run();
+
+        System.out.println("Bye!");
+        ScannerUtil.closeScanner();
     }
 }
